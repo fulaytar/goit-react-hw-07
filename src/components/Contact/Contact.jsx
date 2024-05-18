a06e86423ef7b2b1b96eb62267bf72cbbed4d0b0
fulaytar <fulaytar2405@gmail.com>
2024-05-19 01:24:21 +0300

    add style

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,6 +7,14 @@ import { deleteContact } from "../../redux/contactsOps";
 export default function Contact({ item }) {
   const dispatch = useDispatch();
 
+  const send = (e) => {
+    e.target.style.backgroundColor = "red";
+    e.target.style.color = "white"; // змінюємо колір тексту
+    e.target.style.border = "2px solid darkred"; // додаємо обрамлення
+    e.target.style.transition = "background-color 0.3s, transform 0.2s";
+    dispatch(deleteContact(item.id));
+  };
+
   return (
     <div className={css.container}>
       <h3>
@@ -16,10 +24,7 @@ export default function Contact({ item }) {
         <BsFillTelephoneFill />
         {item.number}
       </p>
-      <button
-        className={css.btn}
-        onClick={() => dispatch(deleteContact(item.id))}
-      >
+      <button className={css.btn} onClick={send}>
         Delete
       </button>
     </div>
